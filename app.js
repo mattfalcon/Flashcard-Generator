dd9240665191628158a0768002f77613f3c6f3e6
Matt Valladarez <mattvalladarez@gmail.com>
2017-05-19 11:27:20 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+//require flashcard
+var flashcard = require('./flaschard.js');
+
+//require cloze
+var cloze = require('./cloze.js');
+
+//require inquire
+var inquirer = require('inquirer');
+
+//require nodemon.js
+
+//require file system module 
+var fs = require('fs');
+
+
+//prompt to select flashcards vs cloze
+inquirer.prompt([{
+
+
+
+}]);
