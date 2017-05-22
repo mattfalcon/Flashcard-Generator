d003ebee6375c34273e3f76b276d9f8052d4164e
Matt Valladarez <mattvalladarez@gmail.com>
2017-05-21 23:54:29 -0500

    Update cloze.js

--- cloze.js
+++ cloze.js
@@ -1,2 +1,25 @@
 //require module
 var fs = require("fs");
+
+module.exports = clozeFlashcard;
+
+
+// constructor for ClozeFlashcard
+function clozeFlashcard (text, cloze) {
+    this.text = text;
+    this.cloze = cloze;
+    this.create = function() {
+        var data = {
+            text: this.text,
+            cloze: this.cloze,
+            type: "cloze",
+        };
+        // add card to log.txt
+        fs.appendFile("log.txt", JSON.stringify(data) + ';', "utf8", function(error) {
+            // if there is an error, log the error
+            if (error) {
+                console.log(error);
+            }
+        });
+    };
+}
