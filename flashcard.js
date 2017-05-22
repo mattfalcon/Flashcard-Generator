b24d5c7fb85b4bd9bd5ae45c1e00e13c00b33c54
Matt Valladarez <mattvalladarez@gmail.com>
2017-05-21 23:55:28 -0500

    Update flashcard.js

--- flashcard.js
+++ flashcard.js
@@ -1,6 +1,24 @@
-
 var fs = require ("fs");
 
 
 
-//flashcard JS constructor
+module.exports = flashcard;
+
+// constructor for BasicFlashcard
+function flashcard(front, back) {
+    this.front = front;
+    this.back = back;
+    this.create = function() {
+        var data = {
+            front: this.front,
+            back: this.back,
+            type: "basic",
+        };
+        // add card to log.txt
+        fs.appendFile("log.txt", JSON.stringify(data) + ';', "utf8", function(error) {
+            if (error) {
+                console.log(error);
+            }
+        });
+    };
+}
